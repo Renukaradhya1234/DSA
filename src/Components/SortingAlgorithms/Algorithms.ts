87754fa15a70828b26607c369eaa45bd81d06933
Renukaradhya T B <renukaradhytb2001@gmail.com>
2024-07-28 12:46:41 +0530

    InserationSort....

--- src/Components/SortingAlgorithms/Algorithms.ts
+++ src/Components/SortingAlgorithms/Algorithms.ts
@@ -33,11 +33,25 @@ class SortingAlgorithms {
         }
     }
 
-    
+    InserationSort(InputArray: Array<number>): void {
+        /*
+            Take the value, and compare with backwards, if it is small place it correct place...
+            We will starts with Second Index by considering first Index is already sorted....
+        */
+        
+        for(var Index: number = 1; Index < InputArray.length; Index ++) {
+            for(var SubIndex: number = Index - 1; SubIndex >= 0; SubIndex --) {
+                if (InputArray[Index] >= InputArray[SubIndex] ) {
+                    break;
+                }
+                [InputArray[Index], InputArray[SubIndex]] = [InputArray[SubIndex], InputArray[Index]];
+            }
+        }
+    }
 }
 
 var bubble = new SortingAlgorithms();
 var Input = [7, 12, 9, 11, 3]
 console.log("before Sorting:- ", Input);
-bubble.SelectionSort(Input);
+bubble.InserationSort(Input);
 console.log("After Sorting:- ", Input);
